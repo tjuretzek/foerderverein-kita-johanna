ce282bdbe2b69a99c194efcd521c35279d93acf9
Tim Juretzek <tim@jur.solutions>
2023-11-21 21:58:16 +0100

    fixed the logo

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import { Suspense } from 'react'
+import { Link } from 'react-router-dom'
 
 import Footer from 'components/Footer'
 import PageMetadata from 'components/PageMetadata'
 import { TITLE } from 'constants/global'
-import { Link } from 'react-router-dom'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,7 +12,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <PageMetadata />
       <header className='w-full flex justify-center'>
         <Link to='/'>
-          <img src='/logo.png' alt={TITLE} className='w-[240px] max-w-full' />
+          <div className='w-[240px] max-w-full'>
+            <Image src='/logo.png' alt={TITLE} height={451} width={640} />
+          </div>
         </Link>
       </header>
       <main className='w-full max-w-screen-lg mx-auto'>
