4e4b196b261c23c979ea73be94af21a0740d9780
Tim Juretzek <tim@jur.solutions>
2023-11-21 21:51:17 +0100

    added logo

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -2,11 +2,18 @@ import { Suspense } from 'react'
 
 import Footer from 'components/Footer'
 import PageMetadata from 'components/PageMetadata'
+import { TITLE } from 'constants/global'
+import { Link } from 'react-router-dom'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <PageMetadata />
+      <header className='w-full flex justify-center'>
+        <Link to='/'>
+          <img src='/logo.png' alt={TITLE} className='w-[240px] max-w-full' />
+        </Link>
+      </header>
       <main className='w-full max-w-screen-lg mx-auto'>
         <Suspense fallback={null}>{children}</Suspense>
       </main>
