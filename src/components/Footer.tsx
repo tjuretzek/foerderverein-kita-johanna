d25500827884f06851abcb5dda8b8bdae81a8e51
Tim Juretzek <tim@jur.solutions>
2024-11-26 15:22:29 +0100

    footer contain

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,3 @@
 export default function Footer() {
-  return <footer className='w-full bg-white md:h-25 h-16 bg-footer md:bg-auto bg-cover'></footer>
+  return <footer className='w-full bg-white md:h-25 h-16 bg-footer bg-contain'></footer>
 }
